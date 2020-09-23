f91c90b2e97aeee8f2d145ea469b338f5e267cf8
sergio <sage_sergio@hotmail.com>
2020-09-23 12:02:07 -0500

    added no delete the entity

--- others/generateStopExpression.py
+++ others/generateStopExpression.py
@@ -3,6 +3,66 @@ import pymysql
 import re
 import simplejson as json
 
+EXPRESSIONS = [
+	r'(.*) \| El Gráfico',
+	r'esquina roja de (.+?) ',
+	r'localidad de (.*)',
+	r'hasta (.*)',
+	r'poblado de (.*)',
+	r'pueblo de (.[^\|]*)',
+	r'colonias como (.*)',
+	r'municipio de (.*)',
+	r'municipio de # (.*)',
+	r'carriles centrales de (.+?)',
+	r'vecindad de (.+?)',
+	r'barrio (.*)', ##Barrio de
+	r'Barrio (.*)', ##Barrio de
+	r'barrio de (.*)',
+	r'barrio de (.*) , ubicada en las calles .*',
+	r'zócalo de (.+?)',
+	r'pileta de panteón de la (.+?)',
+	r'comunidad de (.*) ',
+	r'Cruz de San Juan Ixtayopan en (.+?) ',
+	r'tramo (.* .*)',
+	r'caudal de aguas negras en (.*) \|',
+	r'cadaver al (.+?) ',
+	r'centro de la (.[^\|]*)',
+	r'terreno de (.*)',
+	r'ropero de departamento en (.+?) ',
+	r'penal de (.+?) ',
+	r'Pobladores de (.*)',
+	r'trajinera de (.*)',
+	r'aeropuerto de la (.*)',
+	r'cabecera municipal de (.*)',
+	r'cadáver al Centro de Justicia de (.*)',
+	r'Centro Femenil de Readaptación Social de (.*)',
+	r'Cerrada de Niños Héroes , (.*)',
+	r'chinampas de (.*)',
+	r'Conalep de Tarango (.*)',
+	r'Centro Histórico de la (.*)',
+	r'del Servicio de Urgencias del (.*)',
+	r'deportivo Zarco , de la (.*)',
+	r'Diócesis de (.*)',
+	r'dirección a (.*)',
+	r'Zona Metropolitana del (.*)',
+	r'pirotecnia de San Pablito , en (.*)',
+	r'las de (.+?) ',
+	r'zona de (.*)',
+	r'trajinera de (.+?) ',
+	r'terreno de (.[^\|]*)',
+	r'chelería de (.+?) ',
+	r'puente de la carretera a (.*)',
+	r'deportivo Zarco, de la (.+?) ',
+	r'del servicio de urgencias del (.+?) ',
+	r'caseta de cobro de (.*)',
+	r'el Paseo Bicentenario de (.+?) ',
+	r'cabecera municipal de (.+?) ',
+	r'choque sobre (.+?) ',
+	r'construcción de (.[^\|]*)',
+	r'anciano sobre (.*) ',
+	r'intento de asalto en (.[^\|]*)',
+]
+
 def searchExpressions(text):
 	text = removesigns(text)
 	list_entities = []
@@ -10,120 +70,10 @@ def searchExpressions(text):
 	list_entities = re.findall(r'<START:location> (.+?) <END>',str(text))
 	for entity in list_entities:
 		newEntity = ""
-		if (re.findall(r'(.*) \| El Gráfico',entity)):
-			newEntity = re.findall(r'(.*) \| El Gráfico',entity)
-		if(re.findall(r'esquina roja de (.+?) ', entity)):
-			newEntity = re.findall(r'esquina roja de (.+?) ',entity)
-		if(re.findall(r'localidad de (.*)', entity)):
-			newEntity = re.findall(r'localidad de (.*)',entity)
-		if(re.findall(r'hasta (.*)', entity)):
-			newEntity = re.findall(r'hasta (.*)',entity)
-		if(re.findall(r'poblado de (.*)', entity)):
-			newEntity = re.findall(r'poblado de (.*)',entity)
-		if(re.findall(r'pueblo de (.[^\|]*)', entity)):
-			newEntity = re.findall(r'pueblo de (.[^\|]*)',entity)
-		if(re.findall(r'colonias como (.*)', entity)):
-			newEntity = re.findall(r'colonias como (.*)',entity)
-		if(re.findall(r'municipio de (.*)', entity)):
-			newEntity = re.findall(r'municipio de (.*)',entity)
-		if(re.findall(r'municipio de # (.*)', entity)):
-			newEntity = re.findall(r'municipio de # (.*)',entity)
-		if(re.findall(r'carriles centrales de (.+?)',entity)):
-			newEntity = re.findall(r'carriles centrales de (.+? .+?) ',entity)
-		if(re.findall(r'vecindad de (.+?)', entity)):
-			newEntity = re.findall(r'vecindad de (.+?) ',entity)
-		if(re.findall(r'barrio (.*)', entity)): ##Barrio de
-			newEntity = re.findall(r'barrio (.*)',entity)
-		if(re.findall(r'Barrio (.*)', entity)): ##Barrio de
-			newEntity = re.findall(r'Barrio (.*)',entity)
-		if(re.findall(r'barrio de (.*)', entity)):
-			newEntity = re.findall(r'barrio de (.*)',entity)
-		if(re.findall(r'barrio de (.*) , ubicada en las calles .*', entity)):
-			newEntity = re.findall(r'barrio de (.*) , ubicada en las calles .*',entity)
-		if(re.findall(r'zócalo de (.+?)', entity)):
-			newEntity = re.findall(r'zócalo de (.*)',entity)
-		if(re.findall(r'pileta de panteón de la (.+?)', entity)):
-			newEntity = re.findall(r'pileta de panteón de la (.+? .+?) ',entity)
-		if(re.findall(r'comunidad de (.*) ', entity)):
-			newEntity = re.findall(r' comunidad de (.*) ',entity)
-		if(re.findall(r'Cruz de San Juan Ixtayopan en (.+?) ', entity)):
-			newEntity = re.findall(r'Cruz de San Juan Ixtayopan en (.+?) ',entity)
-		if(re.findall(r'tramo (.* .*)', entity)):
-			newEntity = re.findall(r'tramo (.* .*)',entity)
-		if(re.findall(r'caudal de aguas negras en (.*) \|', entity)):
-			newEntity = re.findall(r'caudal de aguas negras en (.*) \|',entity)
-		if(re.findall(r'cadaver al (.+?) ', entity)):
-			newEntity = re.findall(r'cadaver al (.+?) ',entity)
-		if(re.findall(r'centro de la (.[^\|]*)', entity)):
-			newEntity = re.findall(r'centro de la (.[^\|]*)',entity)
-		if(re.findall(r'terreno de (.*)', entity)):
-			newEntity = re.findall(r'terreno de (.*) ',entity)
-		if(re.findall(r'ropero de departamento en (.+?) ', entity)):
-			newEntity = re.findall(r'ropero de departamento en (.+?) ',entity)
-		if(re.findall(r'penal de (.+?) ', entity)):
-			newEntity = re.findall(r'penal de (.+?) ',entity)
-		if(re.findall(r'Pobladores de (.*)', entity)):
-			newEntity = re.findall(r'Pobladores de (.*)',entity)
-		if(re.findall(r'trajinera de (.*)', entity)):
-			newEntity = re.findall(r'trajinera de (.*)',entity)
-		if(re.findall(r'aeropuerto de la (.*)', entity)):
-			newEntity = re.findall(r'aeropuerto de la (.*)',entity)
-		if(re.findall(r'cabecera municipal de (.*)', entity)):
-			newEntity = re.findall(r'cabecera municipal de (.*)',entity)
-		if(re.findall(r'cadáver al Centro de Justicia de (.*)', entity)):
-			newEntity = re.findall(r'cadáver al Centro de Justicia de (.*)',entity)
-		if(re.findall(r'Centro Femenil de Readaptación Social de (.*)', entity)):
-			newEntity = re.findall(r'Centro Femenil de Readaptación Social de (.*)',entity)
-		if(re.findall(r'Cerrada de Niños Héroes , (.*)', entity)):
-			newEntity = re.findall(r'Cerrada de Niños Héroes , (.*)',entity)
-		if(re.findall(r'chinampas de (.*)', entity)):
-			newEntity = re.findall(r'chinampas de (.*)',entity)
-		if(re.findall(r'Conalep de Tarango (.*)', entity)):
-			newEntity = re.findall(r'Conalep de Tarango (.*)',entity)
-		if(re.findall(r'Centro Histórico de la (.*)', entity)):
-			newEntity = re.findall(r'Centro Histórico de la (.*)',entity)
-		if(re.findall(r'del Servicio de Urgencias del (.*)', entity)):
-			newEntity = re.findall(r'del Servicio de Urgencias del (.*)',entity)
-		if(re.findall(r'deportivo Zarco , de la (.*)', entity)):
-			newEntity = re.findall(r'deportivo Zarco , de la (.*)',entity)
-		if(re.findall(r'Diócesis de (.*)', entity)):
-			newEntity = re.findall(r'Diócesis de (.*)',entity)
-		if(re.findall(r'dirección a (.*)', entity)):
-			newEntity = re.findall(r'dirección a (.*)',entity)
-		if(re.findall(r'Zona Metropolitana del (.*)', entity)):
-			newEntity = re.findall(r'Zona Metropolitana del (.*)',entity)
-		if(re.findall(r'pirotecnia de San Pablito , en (.*)', entity)):
-			newEntity = re.findall(r'pirotecnia de San Pablito , en (.*)',entity)
-		if(re.findall(r'las de (.+?) ', entity)):
-			newEntity = re.findall(r'las de (.+?) ',entity)
-		if(re.findall(r'zona de (.*)', entity)):
-			newEntity = re.findall(r'zona de (.*)',entity)
-		if(re.findall(r'trajinera de (.+?) ', entity)):
-			newEntity = re.findall(r'trajinera de (.+?) ',entity)
-		if(re.findall(r'terreno de (.[^\|]*)', entity)):
-			newEntity = re.findall(r'terreno de (.[^\|]*)',entity)
-		if(re.findall(r'chelería de (.+?) ', entity)):
-			newEntity = re.findall(r'chelería de (.+?) ',entity)
-		if(re.findall(r'puente de la carretera a (.*)', entity)):
-			newEntity = re.findall(r'puente de la carretera a (.*)',entity)
-		if(re.findall(r'deportivo Zarco, de la (.+?) ', entity)):
-			newEntity = re.findall(r'deportivo Zarco, de la (.+?) ',entity)
-		if(re.findall(r'del servicio de urgencias del (.+?) ', entity)):
-			newEntity = re.findall(r'del servicio de urgencias del (.+?) ',entity)
-		if(re.findall(r'caseta de cobro de (.*)', entity)):
-			newEntity = re.findall(r'caseta de cobro de (.*)',entity)
-		if(re.findall(r'el Paseo Bicentenario de (.+?) ', entity)):
-			newEntity = re.findall(r'el Paseo Bicentenario de (.+?) ',entity)
-		if(re.findall(r'cabecera municipal de (.+?) ', entity)):
-			newEntity = re.findall(r'cabecera municipal de (.+?) ',entity)
-		if(re.findall(r'choque sobre (.+?) ', entity)):
-			newEntity = re.findall(r'choque sobre (.+?) ',entity)
-		if(re.findall(r'construcción de (.[^\|]*)', entity)):
-			newEntity = re.findall(r'construcción de (.[^\|]*)',entity)
-		if(re.findall(r'anciano sobre (.*) ', entity)):
-			newEntity = re.findall(r'anciano sobre (.[^\|]*)',entity)
-		if(re.findall(r'intento de asalto en (.[^\|]*)', entity)):
-			newEntity = re.findall(r'intento de asalto en (.+?) ',entity)
+		for expression in EXPRESSIONS:
+			if (re.findall(expression,entity)):
+				newEntity = re.findall(expression,entity)
+		
 		if(newEntity):
 			if(re.findall(r' con (.[^\|]*)', newEntity[0])):
 				newEntity = re.findall(r' con (.[^\|]*) ',newEntity[0])
@@ -131,7 +81,15 @@ def searchExpressions(text):
 				newEntity = re.findall(r'la (.*)',newEntity[0]) 
 			if(re.findall(r' , (.*)',newEntity[0])):
 				newEntity = re.findall(r' , (.*)',newEntity[0]) 
+
 			fin_entity = '<START:location> ' + str(newEntity[0].strip()) + ' <END>'
+
+			#get initial information
+			for match in re.finditer(str(newEntity[0].strip()), str(entity)):
+				start = match.start()
+				end = match.end()
+				fin_entity = entity[:start] + fin_entity + entity[end:]
+				
 			entity = '<START:location> ' + str(entity) + ' <END>'
 			list_changes.update({entity : fin_entity})
 		else:
