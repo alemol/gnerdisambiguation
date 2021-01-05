db294b166736406eae6930320517d58be49bc26c
sergio <sage_sergio@hotmail.com>
2021-01-05 08:49:26 -0600

    udpate disambiguation in class

--- disambiguation_class.py
+++ disambiguation_class.py
@@ -7,44 +7,37 @@ from entity import Entity
 
 key = 'd8c768d5f902425ea0ee628c9984ee4c'
 bounds='-117.07031,14.82841,-86.92383,32.43044'
+
 class Disambiguation:
-	stack
-	conflicts_stack
-	support_stack	
-	success = True
+	success = False
 	resultados = []
 
 	def __init__(self):
-		stack=Stack()
-		conflicts_stack=Stack()
-		support_stack=Stack()
+		self.stack = Stack()
+		self.conflicts_stack = Stack()
+		self.support_stack = Stack()
 	
-	def __delete__(self, instance):
-		es = extractStack()
-		stack.removeAll()
-		conflicts_stack.removeAll()
-		support_stack.removeAll()
+	def __del__(self):
+		self.stack.removeAll()
+		self.conflicts_stack.removeAll()
+		self.support_stack.removeAll()
 
 	def getResultados(self):
-		return resultados
+		return self.resultados
 
 	#############################################################################################
-	def load_entities(list_entity):
-		# file = read_file_as_json(filename)
-		# list_entity = []
-		# text = file["data"][0]["labeled"]
-		# position = 0
-		# list_entity = re.findall(r'<START:location>[\s]+(.+?)[\s]+<END>',text)
-		# print(list_entity)
+	def load_entities(self, text):
+		list_entity = re.findall(r'<START:location>[\s]+(.+?)[\s]+<END>',text)
 		for entity in list_entity:
-			factList=facts_generator(entity, self.stack)
-			#print("factList",factList)
-			#print("entity",entity)
-			clip_s(factList)
+			factList=self.facts_generator(entity, self.stack)
+			print("factList",factList)
+			print("entity",entity)
+			self.clip_s(factList)
 		
+		self.success = True
 		return self.success
 		
-	def facts_generator(nameEntity, stack):
+	def facts_generator(self, nameEntity, stack):
 		factList = []
 		replaceNameEntity = nameEntity.replace(" ", "_")
 		if (stack.isEmpty()):
@@ -57,15 +50,15 @@ class Disambiguation:
 			factsGenerator.stackIsEmpty(factList, stack)
 		return factList
 
-	def clip_s(factList):
+	def clip_s(self, factList):
 		env = clips.Environment()
-		env.define_function(addLocation)
-		env.define_function(stackExchange)
-		env.define_function(associateConflictTop)
-		env.define_function(addConflictsStack)
-		env.define_function(conflictAddStack)
-		env.define_function(addLocationWithAssociation)
-		env.define_function(addLocationWithoutAssociation)
+		env.define_function(self.addLocation)
+		env.define_function(self.stackExchange)
+		env.define_function(self.associateConflictTop)
+		env.define_function(self.addConflictsStack)
+		env.define_function(self.conflictAddStack)
+		env.define_function(self.addLocationWithAssociation)
+		env.define_function(self.addLocationWithoutAssociation)
 		env.load('rules.clp')
 		
 		aux = ''
@@ -83,58 +76,58 @@ class Disambiguation:
 		#print(x)
 		env.reset()
 
-	def addLocation(nameEntity):
-		e = getEntity(nameEntity)
+	def addLocation(self, nameEntity):
+		e = self.getEntity(nameEntity)
 		self.stack.push(e)
 		print("Top de la pila: {0}".format(stack.top().getName()))
 		for ee in stack.entities():
 			print(ee.getName())
 		self.success = False
 
-	def addLocationWithAssociation(nameEntity):
-		e = getEntityWithAssociation(nameEntity)
-		stack.push(e)
+	def addLocationWithAssociation(self, nameEntity):
+		e = self.getEntityWithAssociation(nameEntity)
+		self.stack.push(e)
 		print("Top de la pila: {0}".format(stack.top().getName()))
 		for ee in stack.entities():
 			print(ee.getName())
 
-	def addLocationWithoutAssociation(nameEntity):
-		e = getEntityWithoutAssociation(nameEntity)
-		stack.push(e)
+	def addLocationWithoutAssociation(self, nameEntity):
+		e = self.getEntityWithoutAssociation(nameEntity)
+		self.stack.push(e)
 		print("Top de la pila: {0}".format(stack.top().getName()))
 		for ee in stack.entities():
 			print(ee.getName())
 
-	def stackExchange(nameEntity):
+	def stackExchange(self, nameEntity):
 		replaceNameEntity = nameEntity.replace("_", " ")
-		t = stack.top()
-		stack.pop()
-		e = getEntity(replaceNameEntity)
-		stack.push(e)
+		t = self.stack.top()
+		self.stack.pop()
+		e = self.getEntity(replaceNameEntity)
+		self.getEntityWithAssociationstack.push(e)
 		if(t.getFeatureValue() == 10):
-			n = associateConflictTop(t.getName())
+			n = self.associateConflictTop(t.getName())
 		else:
-			n = getEntityWithAssociation(t.getName())
-			stack.push(n)
+			n = self.getEntityWithAssociation(t.getName())
+			self.stack.push(n)
 		getAlternate(t,n)
-		print("Top de la pila: {0}".format(stack.top().getName()))
-		for ee in stack.entities():
+		print("Top de la pila: {0}".format(self.stack.top().getName()))
+		for ee in self.stack.entities():
 			print(ee.getName())
 
-	def getAlternate(t,n):
+	def getAlternate(self, t,n):
 		t_alternateLongitude = t.getAlternateLongitude()
 		t_alternateLatitude = t.getAlternateLatitude()
 		if (len(t_alternateLatitude)==0):
 			pass
 		else:
 			for element in range(len(t_alternateLatitude)):
-				stack.top().setAlternateLongitude(t_alternateLongitude[element])
-				stack.top().setAlternateLatitude(t_alternateLatitude[element])
-		stack.top().setAlternateLongitude(t.getLongitude())
-		stack.top().setAlternateLatitude(t.getLatitude())
+				self.stack.top().setAlternateLongitude(t_alternateLongitude[element])
+				self.stack.top().setAlternateLatitude(t_alternateLatitude[element])
+		self.stack.top().setAlternateLongitude(t.getLongitude())
+		self.stack.top().setAlternateLatitude(t.getLatitude())
 		#print(stack.top().getName(), stack.top().getAlternateLatitude(), stack.top().getAlternateLongitude())
 
-	def associateConflictTop(nameEntity):
+	def associateConflictTop(self, nameEntity):
 		e = Entity()
 		e.setName(nameEntity)
 		e.setCanonicalName(nameEntity)
@@ -144,104 +137,104 @@ class Disambiguation:
 		#e.setAdmin2code(stack.top().getAdmin2code())
 		e.setLatitude(stack.top().getLatitude())
 		e.setLongitude(stack.top().getLongitude())
-		stack.push(e)
+		self.stack.push(e)
 		print("Top de la pila: {0}".format(stack.top().getName()))
 		#print("ot")
 		#print(stack.top())
 		for ee in stack.entities():
 			print("associateConflictTop " + ee.getName(), ee.getAdmin1code())
 
-	def addConflictsStack(nameEntity):
+	def addConflictsStack(self, nameEntity):
 		e = Entity()
 		e.setName(nameEntity)
 		e.setFeatureCode("Other")
 		e.setFeatureValue("9")
-		conflicts_stack.push(e)
+		self.conflicts_stack.push(e)
 		#print("Pila de Conflictos")
 		#print(conflicts_stack.entities().getName())
 
-	def conflictAddStack():
-		stack_to_support_stack()
-		top = support_stack.top()
-		support_stack.pop()
-		newTop = support_stack.top()
+	def conflictAddStack(self):
+		self.stack_to_support_stack()
+		top = self.support_stack.top()
+		self.support_stack.pop()
+		newTop = self.support_stack.top()
 		while (top.getAdmin1code() == newTop.getAdmin1code()):
-			if (support_stack.isEmpty()):
+			if (self.support_stack.isEmpty()):
 				break
 			else:
-				stack.push(top)
-				top = support_stack.top()
-				support_stack.pop()
-		if (support_stack.isEmpty()):
-			stack.push(top)
-			conflicts_stack_to_stack()
+				self.stack.push(top)
+				top = self.support_stack.top()
+				self.support_stack.pop()
+		if (self.support_stack.isEmpty()):
+			self.stack.push(top)
+			self.conflicts_stack_to_stack()
 		else:
-			conflicts_stack_to_stack()
-			stack.push(top)
+			self.conflicts_stack_to_stack()
+			self.stack.push(top)
 		try:
-			support_stack_to_stack()
+			self.support_stack_to_stack()
 		except Exception as e:
 				pass
 		#for entities in stack.entities():
 		#	print("stack",entities.getName())
 
-	def stack_to_support_stack():
-		for i in range(stack.getSize()):
-			if (stack.isEmpty()):
+	def stack_to_support_stack(self):
+		for i in range(self.stack.getSize()):
+			if (self.stack.isEmpty()):
 				pass
 			else:
-				top = stack.top()
-				stack.pop()
-				support_stack.push(top)
+				top = self.stack.top()
+				self.stack.pop()
+				self.support_stack.push(top)
 
-	def support_stack_to_stack():
-		for i in range(support_stack.getSize()):
-			if (support_stack.isEmpty()):
+	def support_stack_to_stack(self):
+		for i in range(self.support_stack.getSize()):
+			if (self.support_stack.isEmpty()):
 				pass
 			else:
-				top = support_stack.top()
-				support_stack.pop()
-				stack.push(top)
+				top = self.support_stack.top()
+				self.support_stack.pop()
+				self.stack.push(top)
 
-	def conflicts_stack_to_stack():
+	def conflicts_stack_to_stack(self):
 		for conflicts in conflicts_stack.entities():
 			e = Entity()
 			e.setName(conflicts.getName())
 			e.setCanonicalName(conflicts.getName())
-			e.setFeatureCode(stack.top().getFeatureCode())
+			e.setFeatureCode(self.stack.top().getFeatureCode())
 			e.setFeatureValue(conflicts.getFeatureValue())
-			e.setAdmin1code(stack.top().getAdmin1code())
-			e.setLatitude(stack.top().getLatitude())
-			e.setLongitude(stack.top().getLongitude())
-			stack.push(e)
-		conflicts_stack.removeAll()
+			e.setAdmin1code(self.stack.top().getAdmin1code())
+			e.setLatitude(self.stack.top().getLatitude())
+			e.setLongitude(self.stack.top().getLongitude())
+			self.stack.push(e)
+		self.conflicts_stack.removeAll()
 
-	def getEntity(nameEntity):
+	def getEntity(self, nameEntity):
 		geocoder = OpenCageGeocode(key)
 		response = geocoder.geocode(nameEntity, countrycode= 'mx', language='es', bounds = bounds, limit = 1, no_annotations = 1)
-		e = addEntityObject(response,nameEntity)
+		e = self.addEntityObject(response,nameEntity)
 		return e
 
-	def getEntityWithAssociation(nameEntity):
-		top_FeatureValue = stack.top().getFeatureValue()
-		top_admin1Code = stack.top().getAdmin1code()
-		boundsLocal = '{0},{1}'.format(stack.top().getLatitude(),stack.top().getLongitude())
+	def getEntityWithAssociation(self, nameEntity):
+		top_FeatureValue = self.stack.top().getFeatureValue()
+		top_admin1Code = self.stack.top().getAdmin1code()
+		boundsLocal = '{0},{1}'.format(self.stack.top().getLatitude(),self.stack.top().getLongitude())
 		geocoder = OpenCageGeocode(key)
 		query = geocoder.geocode(nameEntity, countrycode= 'mx', language='es', bounds = boundsLocal, limit = 5, no_annotations = 1)
 		if (query):
-			e = addEntityObject(query,nameEntity)
+			e = self.addEntityObject(query,nameEntity)
 		else:
 			query2 = geocoder.geocode(nameEntity, countrycode= 'mx', language='es', bounds = bounds, limit = 1, no_annotations = 1, proximity = boundsLocal)
-			e = addEntityObject(query2,nameEntity)
+			e = self.addEntityObject(query2,nameEntity)
 		return e
 
-	def getEntityWithoutAssociation(nameEntity):
+	def getEntityWithoutAssociation(self, nameEntity):
 		geocoder = OpenCageGeocode(key)
 		query = geocoder.geocode(nameEntity, countrycode= 'mx', language='es', bounds = bounds, limit = 1, no_annotations = 1)
-		e = addEntityObject(query,nameEntity)
+		e = self.addEntityObject(query,nameEntity)
 		return e
 
-	def addEntityObject(response,nameEntity):
+	def addEntityObject(self, response,nameEntity):
 		#print("addEntityObject")
 		e = Entity()
 	#	print("uno")
@@ -258,18 +251,23 @@ class Disambiguation:
 		e.setLatitude(response[0]["geometry"]["lat"])
 		e.setLongitude(response[0]["geometry"]["lng"])
 		e.setName(nameEntity)
-		feature_code, feature_value = fvalue(response[0]["components"]["_type"])
+		feature_code, feature_value = self.fvalue(response[0]["components"]["_type"])
 		e.setFeatureValue(feature_value)
 		e.setFeatureCode(feature_code)
 		return e
 
-	def fvalue(featureV):
+	def fvalue(self, featureV):
 		hlevel = hierarchical_level.dic_level()
 		for key, value in hlevel.items():
 			if(featureV == key):
 				return key, value
 
 def main():
+	text = "Los <START:location> Tacos de doña Lupe <END>, la  <START:location> Panaderia el Rosal <END> y la <START:location> Ferreteria el clavo <END> están registrados en el municipio de <START:location> Guerrero <END> , <START:location> Tamaulipas <END>"
 	initObject = Disambiguation()
-	resultados = initObject.load_entities(listEntity)
-	remove(initObject)
+	resultados = initObject.load_entities(text)
+	print("GOOD?",resultados)
+	del initObject
+
+if __name__ == '__main__':
+	main()
